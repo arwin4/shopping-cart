80057f8848c8d9a85b44af2946599644c447e3c9
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-03 16:53:33 +0200

    Fetch shop items from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from '../styles/App.module.css';
 
 function App() {
+  const [shopItems, setShopItems] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(
+    () =>
+      async function fetchShopItems() {
+        try {
+          const response = await fetch('https://fakestoreapi.com/products');
+          if (!response.ok) {
+            throw new Error('Unable to fetch shop items from Fake Store API');
+          }
+          const fetchedShopItems = await response.json();
+          setShopItems(fetchedShopItems);
+          setError(null); // Prevent error state persisting
+          console.log(fetchedShopItems);
+        } catch (err) {
+          setError(err.message);
+          setShopItems(null);
+          console.log(err);
+        } finally {
+          setLoading(false);
+        }
+      },
+    [],
+  );
+
   return (
     <div>
       <h1 className={styles.title}>Template</h1>
