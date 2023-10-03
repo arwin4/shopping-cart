35bee0f685610472ad24535a60f99db765bc5bc5
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-03 14:36:29 +0200

    Use CSS modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import styles from '../styles/App.module.css';
+
 function App() {
-  return <h1>Template</h1>;
+  return (
+    <div>
+      <h1 className={styles.title}>Template</h1>
+    </div>
+  );
 }
 
 export default App;
