9d7ed8cd6c0186a2945fe626e46dbe5fb94e5b3f
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-03 14:15:58 +0200

    Setup project

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+describe('App component', () => {
+  it('renders correct heading', () => {
+    render(<App />);
+    expect(screen.getByRole('heading').textContent).toMatch(/Template/i);
+  });
+});
