fab69c64ef83ea550e75afc721e5bbbbedc8dcff
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-05 11:42:07 +0200

    Render cart items on Cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,34 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import ShopContext from '../shopContext';
 
 export default function Cart() {
   const { cartItems } = useContext(ShopContext);
-  console.log(cartItems);
-  return 'Nothing yet';
+
+  // Get an array of items and the amount of each item, for example: [[{'Backpack', ...}, 3], ...]
+  function getCountedCartItems() {
+    // Source for duplicate counting:
+    // https://bobbyhadz.com/blog/javascript-count-duplicates-in-array
+    const uniqueItems = [...new Set(cartItems)];
+    return uniqueItems.map((item) => [
+      item,
+      cartItems.filter((cartItem) => cartItem === item).length,
+    ]);
+  }
+  const countedCartItems = getCountedCartItems();
+
+  return (
+    <>
+      {countedCartItems.map((item) => {
+        const itemData = item[0];
+        const itemCount = item[1];
+        return (
+          <div key={itemData.id} className="cart-item">
+            {itemData.title}: {itemCount}
+          </div>
+        );
+      })}
+      <Link to="/">Back to store</Link>
+    </>
+  );
 }
