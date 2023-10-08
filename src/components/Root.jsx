a56b937270fac96344c3b6ee6b972f1aec0b8643
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-08 16:51:49 +0200

    Fix API fetching on unmount, not once during mount

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -6,29 +6,28 @@ function Root() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(
-    () =>
-      async function fetchShopItems() {
-        try {
-          const response = await fetch('https://fakestoreapi.com/products');
-          if (!response.ok) {
-            throw new Error('Unable to fetch shop items from Fake Store API');
-          }
-          const fetchedShopItems = await response.json();
-          // const fetchedShopItems = APImock;
-          setProducts(fetchedShopItems);
-          setError(null); // Prevent error state persisting
-          console.log(fetchedShopItems);
-        } catch (err) {
-          setError(err.message);
-          setProducts(null);
-          console.log(err);
-        } finally {
-          setLoading(false);
+  useEffect(() => {
+    const fetchShopItems = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+          throw new Error('Unable to fetch shop items from Fake Store API');
         }
-      },
-    [],
-  );
+        const fetchedShopItems = await response.json();
+        // const fetchedShopItems = APImock;
+        setProducts(fetchedShopItems);
+        setError(null); // Prevent error state persisting
+        console.log(fetchedShopItems);
+      } catch (err) {
+        setError(err.message);
+        setProducts(null);
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchShopItems();
+  }, []);
 
   if (error) return <>There was an error: {error}</>;
   if (loading) return <>Loading...</>;
