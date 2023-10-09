c227ac12260080bf7a1658e20b5484bbce20abf2
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-09 14:14:09 +0200

    Add styling for remove item button

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
+import { Trash } from 'lucide-react';
 import CartContext from '../context/cartContext';
 import NavBar from './NavBar';
+import styles from '../styles/Button.module.css';
 
 export default function Cart() {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, handleRemoveFromCart } = useContext(CartContext);
 
   // Get an array of items and the amount of each item, for example: [[{'Backpack', ...}, 3], ...]
   function getCountedCartItems() {
@@ -23,6 +25,13 @@ export default function Cart() {
     return (
       <div key={itemData.id} className="cart-item">
         {itemData.title}: {itemCount}
+        <button
+          type="button"
+          className={styles.btn}
+          onClick={() => handleRemoveFromCart(itemData.id)}
+        >
+          <Trash icon="lucide:edit" />
+        </button>
       </div>
     );
   });
