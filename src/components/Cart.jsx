e2bcb833f3b9d64594337febecf76aebec945abb
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-10 16:51:22 +0200

    Style Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,36 +2,53 @@ import React, { useContext } from 'react';
 import { Minus, Plus, Trash } from 'lucide-react';
 import CartContext from '../context/cartContext';
 import NavBar from './NavBar';
-import styles from '../styles/Button.module.css';
+
+// CSS
+import { normal } from '../styles/Button.module.css';
+import {
+  summary,
+  cartItem,
+  thumb,
+  number,
+  buttonWrapper,
+  plusMinus,
+  remove,
+} from '../styles/cartSummary.module.css';
 
 export default function Cart() {
   const { cartItems, addToCart, removeAllFromCart, removeOneFromCart } =
     useContext(CartContext);
 
   const cartSummary = cartItems.map((item) => (
-    <div key={item.id} className="cart-item">
-      {item.title}: {item.numberInCart}
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={() => removeOneFromCart(item.id)}
-      >
-        <Minus />
-      </button>{' '}
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={() => addToCart(item.id)}
-      >
-        <Plus />
-      </button>
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={() => removeAllFromCart(item.id)}
-      >
-        <Trash />
-      </button>
+    <div key={item.id} className={cartItem}>
+      <div className="title">{item.title}</div>
+      <img className={thumb} src={item.image} />
+      <div className={number}>{item.numberInCart}</div>
+      <div className={buttonWrapper}>
+        <div className={plusMinus}>
+          <button
+            type="button"
+            className={normal}
+            onClick={() => addToCart(item.id)}
+          >
+            <Plus />
+          </button>{' '}
+          <button
+            type="button"
+            className={normal}
+            onClick={() => removeOneFromCart(item.id)}
+          >
+            <Minus />
+          </button>{' '}
+        </div>
+        <button
+          type="button"
+          className={`${normal} ${remove}`}
+          onClick={() => removeAllFromCart(item.id)}
+        >
+          <Trash />
+        </button>
+      </div>
     </div>
   ));
 
@@ -47,8 +64,10 @@ export default function Cart() {
   return (
     <>
       <NavBar />
-      {cartItems.length === 0 ? 'Your shopping cart is empty.' : cartSummary}
-      {totalPrice}
+      <div className={summary}>
+        {cartItems.length === 0 ? 'Your shopping cart is empty.' : cartSummary}
+        Total price {totalPrice}
+      </div>
     </>
   );
 }
