cba947e3a6455ef805d420d2e176d7ea1a381e66
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-11 13:17:47 +0200

    Style storefront

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,22 +1,38 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { ShoppingBasket } from 'lucide-react';
 import CartContext from '../context/cartContext';
+
+// CSS
+import {
+  store,
+  productImage,
+  productContainer,
+} from '../styles/Store.module.css';
+import { normal } from '../styles/Button.module.css';
 
 export default function Store({ allProducts }) {
   const products = allProducts;
-  const { addToCart, removeAllFromCart } = useContext(CartContext);
+  const { addToCart } = useContext(CartContext);
 
   return (
-    <div className="item-overview">
+    <div className={store}>
       {products.map((product) => (
-        <div key={product.id}>
+        <div className={productContainer} key={product.id}>
           {product.title}
-          <button type="button" onClick={() => addToCart(product.id)}>
-            Add to cart
+          <img
+            className={productImage}
+            src={product.image}
+            alt={product.title}
+          />
+          €{product.price.toFixed(2)}
+          <button
+            className={normal}
+            type="button"
+            onClick={() => addToCart(product.id)}
+          >
+            <ShoppingBasket />
           </button>{' '}
-          <button type="button" onClick={() => removeAllFromCart(product.id)}>
-            Remove from cart
-          </button>
         </div>
       ))}
     </div>
